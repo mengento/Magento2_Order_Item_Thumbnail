ca9fbe3ad10d07f5451e99cd86bc2cfba87aa0d0
mengento <mallwell@qq.com>
2015-12-05 12:29:09 +0800

    Create DefaultRenderer.php

---
+++ Block/Adminhtml/Order/View/Items/Renderer/DefaultRenderer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mengento\Thumbnail\Block\Adminhtml\Order\View\Items\Renderer;
+
+class DefaultRenderer extends \Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer
+{
+
+}
